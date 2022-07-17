40438c380ce28ca0e4acff2135089a5edfe51a91
yanis <yanis.legrand.1@gmail.com>
2022-07-17 16:46:53 +0200

    test touch event

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -259,6 +259,7 @@ function audioError(){
 
 
 // Test toutch event
+// TODO 
 
 document.addEventListener('touchstart', function(e){
   console.log(e.target);
@@ -267,6 +268,53 @@ document.addEventListener('touchstart', function(e){
   }
 })
 
-document.addEventListener('touchmove', function(e){
-  console.log(e);
-})
+document.addEventListener('touchstart', handleTouchStart, false);        
+document.addEventListener('touchmove', handleTouchMove, false);
+
+var xDown = null;                                                        
+var yDown = null;
+
+function getTouches(evt) {
+  return evt.touches ||             // browser API
+         evt.originalEvent.touches; // jQuery
+}                                                     
+                                                                         
+function handleTouchStart(evt) {
+    const firstTouch = getTouches(evt)[0];                                      
+    xDown = firstTouch.clientX;                                      
+    yDown = firstTouch.clientY;                                      
+};                                                
+                                                                         
+function handleTouchMove(evt) {
+    if ( ! xDown || ! yDown ) {
+        return;
+    }
+
+    var xUp = evt.touches[0].clientX;                                    
+    var yUp = evt.touches[0].clientY;
+
+    var xDiff = xDown - xUp;
+    var yDiff = yDown - yUp;
+                                                                         
+    if ( Math.abs( xDiff ) > Math.abs( yDiff ) ) {/*most significant*/
+        if ( xDiff > 0 ) {
+            /* right swipe */ 
+            console.log("right swipe : next song")
+            nextSong()
+            
+        } else {
+            /* left swipe */
+            console.log("left swipe : prev song")
+            prevSong()
+        }                       
+    } else {
+        if ( yDiff > 0 ) {
+            /* down swipe */ 
+        } else { 
+            /* up swipe */
+        }                                                                 
+    }
+    /* reset values */
+    xDown = null;
+    yDown = null;                                             
+};
